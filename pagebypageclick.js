af0dfc686441a9fe8a9f10f74a2e48ee37b5bdab
Timothy Torgenrud <torg@stanford.edu>
2014-02-11 17:35:47 -0800

    Click event binding example

---
+++ pagebypageclick.js
@@ -0,0 +1,36 @@
+var counter;
+
+$(document).on('pagecreate','#first_page', function() {
+//     alert('Pagecreate fired');
+});
+
+$(document).on('pageinit','#first_page',function() {
+     counter = 1;
+});
+
+$(document).on('pagebeforeshow','#first_page',function(event, ui)
+{
+	if (counter >1){
+		$('#pageonetextline').append(' and forth');
+	}
+});
+$(document).on('pageshow','#first_page',function(event, ui)
+{
+	alert('Pageshow fired and counter is ' + counter);
+	counter = counter + 1;
+});
+$(document).on('pagebeforehide','#first_page',function(event, ui)
+{
+//	alert('Pagebeforehide fired');
+});
+$(document).on('pagehide','#first_page',function(event, ui)
+{
+	$('#pageonetextline').append(' and back');
+});
+$(document).on('pagehide','#second_page',function(event, ui)
+{
+	$(document).on('click','#pageoneanchor',function(event,ui))
+	{
+		alert("Hopped back to page one!");
+	});
+});
